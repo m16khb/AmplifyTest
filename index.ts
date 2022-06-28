b5732a81dbef65507f1cd0cedd95b6b12977e990
김하빈 <m16khb@gmail.com>
2022-06-28 09:41:27 +0900

    modify

--- index.ts
+++
@@ -1,8 +0,0 @@
-let date = new Date();
-let offset = date.getTimezoneOffset() * 60000;
-let dateOffset = new Date(date.getTime() - offset);
-let now = dateOffset.toISOString();
-
-console.log('hello');
-console.log(now);
-process.env.TZ = 'Asia/Seoul';
